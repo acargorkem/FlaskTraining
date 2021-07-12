e44dffec701fd85f23088042fb76d948b038c6c1
acargorkem <acargorkem@outlook.com>
2021-07-13 00:34:31 +0300

    SQLAlchemy, SQLite

--- app.py
+++ app.py
@@ -1,21 +1,17 @@
- from flask import Flask
+from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
 
-
-from db import db
 from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
-
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'gorkem'
 api = Api(app)
-
 
 jwt = JWT(app, authenticate, identity)  # /auth
 
@@ -26,5 +22,7 @@ api.add_resource(Store, '/store/<string:name>')
 api.add_resource(StoreList, '/stores')
 
 if __name__ == '__main__':
+    from db import db
+
     db.init_app(app)
     app.run(host='0.0.0.0', port=5000, debug=True)
