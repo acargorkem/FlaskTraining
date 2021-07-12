7df91d608ef667db161dcc2702ba4f9955cb92f9
acargorkem <acargorkem@outlook.com>
2021-07-13 00:30:12 +0300

    SQLAlchemy, SQLite

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+ from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
 
@@ -15,11 +15,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'gorkem'
 api = Api(app)
-
-
-@app.before_first_request
-def create_tables():
-    db.create_all()
 
 
 jwt = JWT(app, authenticate, identity)  # /auth
