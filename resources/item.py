8092965c32b151db04f854d5ba359d083c6c999a
acargorkem <acargorkem@outlook.com>
2021-07-13 14:06:04 +0300

    User model, code refactor

--- resources/item.py
+++ resources/item.py
@@ -1,6 +1,5 @@
-import sqlite3
 from flask_restful import Resource, reqparse
-from flask_jwt import jwt_required
+from flask_jwt_extended import jwt_required
 from models.item import ItemModel
 
 
@@ -9,7 +8,7 @@ class Item(Resource):
     parser.add_argument('price', type=float, required=True, help="Price field cannot be left blank")
     parser.add_argument('store_id', type=int, required=True, help="Store_id field cannot be left blank")
 
-    @jwt_required()
+    @jwt_required
     def get(self, name):
         item = ItemModel.find_by_name(name)
         if item:
--- resources/user.py
+++ resources/user.py
@@ -1,14 +1,18 @@
+import hmac
+
 from flask_restful import Resource, reqparse
+from flask_jwt_extended import create_access_token, create_refresh_token
 from models.user import UserModel
+
+_user_parser = reqparse.RequestParser()
+_user_parser.add_argument('username', type=str, required=True, help="Username cannot be blank")
+_user_parser.add_argument('password', type=str, required=True, help="Password cannot be blank")
 
 
 class UserRegister(Resource):
-    parser = reqparse.RequestParser()
-    parser.add_argument('username', type=str, required=True, help="Username cannot be blank")
-    parser.add_argument('password', type=str, required=True, help="Password cannot be blank")
 
     def post(self):
-        data = UserRegister.parser.parse_args()
+        data = _user_parser.parse_args()
 
         if UserModel.find_by_username(data['username']):
             return {'message': 'User already exists'}, 400
@@ -36,3 +40,21 @@ class User(Resource):
 
         user.delete_from_db()
         return {'message': 'User deleted'}, 200
+
+
+class UserLogin(Resource):
+
+    @classmethod
+    def post(cls):
+        data = _user_parser.parse_args()
+        user = UserModel.find_by_username(data['username'])
+
+        if user and hmac.compare_digest(user.password, data['password']):
+            access_token = create_access_token(identity=user.id, fresh=True)
+            refresh_token = create_refresh_token(user.id)
+            return {
+                       'access_token': access_token,
+                       'refresh_token': refresh_token
+                   }, 200
+
+        return {'message': 'Invalid credentials'}, 401
