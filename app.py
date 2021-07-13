89f3d319683270596f1f9dd395a7e5422a9191cd
acargorkem <acargorkem@outlook.com>
2021-07-13 13:22:44 +0300

    User model, code refactor

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_restful import Api
 from flask_jwt import JWT
 
 from security import authenticate, identity
-from resources.user import UserRegister
+from resources.user import UserRegister, User
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
@@ -19,6 +19,7 @@ if uri.startswith("postgres://"):
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['PROPAGATE_EXCEPTIONS'] = True
 app.secret_key = 'gorkem'
 api = Api(app)
 
@@ -29,6 +30,7 @@ api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
 api.add_resource(Store, '/store/<string:name>')
 api.add_resource(StoreList, '/stores')
+api.add_resource(User, '/user<int:user_id>')
 
 if __name__ == '__main__':
     from db import db
